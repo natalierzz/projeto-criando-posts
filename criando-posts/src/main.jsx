f059ae23ea9d7fc520805a5b1f5dd4aed8b02f1e
Natali Rodrigues <natalieliza1705@gmail.com>
2025-06-08 18:45:50 -0300

    feat: Implementa roteamento da aplicação com react-router-dom
    feat: Implementa roteamento da aplicação com react-router-dom

--- criando-posts/src/main.jsx
+++ criando-posts/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+import {createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./index.css";
+
+//páginas
+import Home from './routes/Home.jsx';
+import NewPost from './routes/NewPost.jsx';
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children:[{
+      path: "/",
+      element:<Home/>
+    },
+    {
+      path: "/new",
+      element:<NewPost/>
+    }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
