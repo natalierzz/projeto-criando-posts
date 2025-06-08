6c5b5f1195ff273f7fd2c74dc78f9cb4c4fb25cb
Natali Rodrigues <natalieliza1705@gmail.com>
2025-06-08 18:49:08 -0300

    feat: Configura componente App para roteamento
    Integra o componente Outlet do react-router-dom no App.jsx
    para permitir a renderização dinâmica das rotas. Remove o título
    estático e adiciona um contêiner para o conteúdo das páginas.

--- criando-posts/src/App.jsx
+++ criando-posts/src/App.jsx
@@ -1,10 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-    <h1>Projeto React</h1>
+      <div className="conteiner" >
+        <Outlet/>
+      </div>
+
     </div>
   );
 }
