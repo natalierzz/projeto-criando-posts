3d1b357a795c0bfe1474e6b63761566c1e2b1d75
Natali Rodrigues <natalieliza1705@gmail.com>
2025-06-09 22:29:59 -0300

    feat: Adiciona Box do Material UI no App.jsx
     Importa o Box do Material UI e substitui a div principal por ele no App.jsx, é o início da integração do Material UI no  projeto.

--- criando-posts/src/App.jsx
+++ criando-posts/src/App.jsx
@@ -3,16 +3,18 @@ import { Outlet } from 'react-router-dom';
 
 import './App.css'
 
+import Box from '@mui/material/Box';
+
 function App() {
 
   return (
-    <div className="App">
+    <Box className="App"> 
       <Navbar/>
       <div className="conteiner" >
         <Outlet/>
       </div>
 
-    </div>
+    </Box>
   );
 }
 
